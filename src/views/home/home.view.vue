d1d0a5d9bf6cef49f544dea0059710b8bd21c48c
Tomasz Dudek <tomasz.dudek@antologic.com>
2022-07-31 00:49:17 +0200

    feat: add input component

--- src/views/home/home.view.vue
+++ src/views/home/home.view.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import mainTemplate from '@/views/main-template/main-template.view.vue';
+import cInput from '@/components/input/input.component.vue';
 </script>
 
 <template>
   <main-template />
   <div>Home</div>
+  <c-input label="Kod rabatowy"
+           :show-approve="true" />
 </template>
 
 <style lang="scss" scoped></style>
