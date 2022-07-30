c75ae4ab128cbf8d44390a3809e569e843bfbbea
Tomasz Dudek <tomasz.dudek@antologic.com>
2022-07-30 14:43:49 +0200

    eslint: change version

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-// import main-template from "@/views/main-template/main-template.view.vue";
 import vHome from "@/views/home/home.view.vue";
 import dummyLogo from "@/assets/dummy-logo.vue";
 </script>
